c9bec2487af28e38ae829057a40183736fcdeba3
JP Strydom <jpstrydom8@gmail.com>
2018-02-26 00:29:30 +0200

    Fix a test

---
+++ src/test/apply-config-file.spec.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const { getRootPath } = require('../generator-utilities');
+
+const applyConfig = require('../apply-config-file');
+
+const ROOT_PATH = getRootPath();
+
+describe('Apply config file - Unit Test', () => {
+    describe('applyConfig', () => {
+        it('should apply config file parameters to passed in parameters', () => {
+            // TODO: Needs some tests
+        });
+    });
+});
--- src/test/create-all-templates.spec.js
+++ src/test/create-all-templates.spec.js
@@ -2,6 +2,7 @@
 
 const fs = require('fs');
 const path = require('path');
+const shell = require('shelljs');
 
 const { getRootPath } = require('../generator-utilities');
 
