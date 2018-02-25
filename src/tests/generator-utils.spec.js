916cd21b08f08e1f469a51c6f5b5104d809e6e6d
Muzikayise Flynn Buthelezi (zuluCoda) <muzi@mfbproject.co.za>
2018-02-25 16:28:48 +0200

    [muzi] adding tests for generating code for native without redux :fist:

--- src/tests/generator-utils.spec.js
+++ src/tests/generator-utils.spec.js
@@ -14,107 +14,254 @@ const { getRootPath, getAllDirectories } = require('../generator-utils');
 
 const ROOT_PATH = getRootPath();
 
-
 describe('Generator Utils - Unit Test', () => {
-  describe('getAllDirectories', () => {
-    describe('for getReactComponentDirs', () => {
-      describe('for web', () => {
-        describe('NOT redux', () => {
-          it('should return all templates with directories set name and create folder name, with test', () => {
-            const name = 'some_name';
-            const directory = 'some_directory';
+    describe('getAllDirectories', () => {
+        describe('for getReactComponentDirs', () => {
+            describe('for web', () => {
+                describe('NOT redux', () => {
+                    it('should return all templates with directories set name and create folder name, with test', () => {
+                        const name = 'some_name';
+                        const directory = 'some_directory';
 
-            const actual = getAllDirectories(name, directory);
+                        const actual = getAllDirectories(name, directory);
 
-            const templates = path.join(ROOT_PATH,'templates', 'web', 'react');
-            const folderName = name;
-            const expected = {
-              view: {
-                template: path.join(templates, 'template.view.js'),
-                generated: path.join(ROOT_PATH, directory, folderName, `${name}.view.js`)
-              },
-              viewTest: {
-                template: path.join(templates, 'test', 'template.view.spec.js'),
-                generated: path.join(ROOT_PATH, directory, folderName, 'test', `${name}.view.spec.js`)
-              },
-              stylesheet: {
-                template: path.join(templates, '_template.styles.scss'),
-                generated: path.join(ROOT_PATH, directory, folderName, `_${name}.styles.scss`)
-              }
-            };
+                        const templates = path.join(ROOT_PATH, 'templates', 'web', 'react');
+                        const folderName = name;
+                        const expected = {
+                            view: {
+                                template: path.join(templates, 'template.view.js'),
+                                generated: path.join(
+                                    ROOT_PATH,
+                                    directory,
+                                    folderName,
+                                    `${name}.view.js`
+                                )
+                            },
+                            viewTest: {
+                                template: path.join(templates, 'test', 'template.view.spec.js'),
+                                generated: path.join(
+                                    ROOT_PATH,
+                                    directory,
+                                    folderName,
+                                    'test',
+                                    `${name}.view.spec.js`
+                                )
+                            },
+                            stylesheet: {
+                                template: path.join(templates, '_template.styles.scss'),
+                                generated: path.join(
+                                    ROOT_PATH,
+                                    directory,
+                                    folderName,
+                                    `_${name}.styles.scss`
+                                )
+                            }
+                        };
+
+                        expect(actual).toEqual(expected);
+                        expect(
+                            fs.existsSync(path.join(ROOT_PATH, directory, folderName))
+                        ).toBeTruthy();
+                    });
+                });
+
+                describe('WITH redux', () => {
+                    it('should return all templates with directories set name and create folder name, with test', () => {
+                        const name = 'some_name_redux';
+                        const directory = 'some_directory_redux';
+
+                        const actual = getAllDirectories(name, directory, false, true);
+
+                        const templates = path.join(ROOT_PATH, 'templates', 'web', 'react-redux');
+                        const folderName = name;
+                        const expected = {
+                            view: {
+                                template: path.join(templates, 'template.view.js'),
+                                generated: path.join(
+                                    ROOT_PATH,
+                                    directory,
+                                    folderName,
+                                    `${name}.view.js`
+                                )
+                            },
+                            viewTest: {
+                                template: path.join(templates, 'test', 'template.view.spec.js'),
+                                generated: path.join(
+                                    ROOT_PATH,
+                                    directory,
+                                    folderName,
+                                    'test',
+                                    `${name}.view.spec.js`
+                                )
+                            },
+                            stylesheet: {
+                                template: path.join(templates, '_template.styles.scss'),
+                                generated: path.join(
+                                    ROOT_PATH,
+                                    directory,
+                                    folderName,
+                                    `_${name}.styles.scss`
+                                )
+                            },
+                            container: {
+                                template: path.join(templates, 'template.container.js'),
+                                generated: path.join(
+                                    ROOT_PATH,
+                                    directory,
+                                    folderName,
+                                    `${name}.container.js`
+                                )
+                            },
+                            containerTest: {
+                                template: path.join(
+                                    templates,
+                                    'test',
+                                    'template.container.spec.js'
+                                ),
+                                generated: path.join(
+                                    ROOT_PATH,
+                                    directory,
+                                    folderName,
+                                    'test',
+                                    `${name}.container.spec.js`
+                                )
+                            },
+
+                            reducer: {
+                                template: path.join(templates, 'template.reducer.js'),
+                                generated: path.join(
+                                    ROOT_PATH,
+                                    directory,
+                                    folderName,
+                                    `${name}.reducer.js`
+                                )
+                            },
+                            reducerTest: {
+                                template: path.join(templates, 'test', 'template.reducer.spec.js'),
+                                generated: path.join(
+                                    ROOT_PATH,
+                                    directory,
+                                    folderName,
+                                    'test',
+                                    `${name}.reducer.spec.js`
+                                )
+                            }
+                        };
+
+                        expect(actual).toEqual(expected);
+                        expect(
+                            fs.existsSync(path.join(ROOT_PATH, directory, folderName))
+                        ).toBeTruthy();
+                    });
+                });
+            });
+
+            describe('for native', () => {
+                describe('NOT redux', () => {
+                    it('should return all templates with directories set name and create folder name, with test', () => {
+                        const name = 'some_name_native';
+                        const directory = 'some_directory_native';
+
+                        const actual = getAllDirectories(name, directory, true);
+
+                        const templates = path.join(ROOT_PATH, 'templates', 'native', 'react');
+                        const folderName = name;
+                        const expected = {
+                            view: {
+                                template: path.join(templates, 'template.view.js'),
+                                generated: path.join(
+                                    ROOT_PATH,
+                                    directory,
+                                    folderName,
+                                    `${name}.view.js`
+                                )
+                            },
+                            viewTest: {
+                                template: path.join(templates, 'test', 'template.view.spec.js'),
+                                generated: path.join(
+                                    ROOT_PATH,
+                                    directory,
+                                    folderName,
+                                    'test',
+                                    `${name}.view.spec.js`
+                                )
+                            }
+                        };
+
+                        expect(actual).toEqual(expected);
+                        expect(
+                            fs.existsSync(path.join(ROOT_PATH, directory, folderName))
+                        ).toBeTruthy();
+                    });
+                });
+
+                // describe('WITH redux', () => {
+                //   it('should return all templates with directories set name and create folder name, with test', () => {
+                //     const name = 'some_name_redux';
+                //     const directory = 'some_directory_redux';
+                //
+                //     const actual = getAllDirectories(name, directory, false, true);
+                //
+                //     const templates = path.join(ROOT_PATH,'templates', 'web', 'react-redux');
+                //     const folderName = name;
+                //     const expected = {
+                //       view: {
+                //         template: path.join(templates, 'template.view.js'),
+                //         generated: path.join(ROOT_PATH, directory, folderName, `${name}.view.js`)
+                //       },
+                //       viewTest: {
+                //         template: path.join(templates, 'test', 'template.view.spec.js'),
+                //         generated: path.join(ROOT_PATH, directory, folderName, 'test', `${name}.view.spec.js`)
+                //       },
+                //       stylesheet: {
+                //         template: path.join(templates, '_template.styles.scss'),
+                //         generated: path.join(ROOT_PATH, directory, folderName, `_${name}.styles.scss`)
+                //       },
+                //       container: {
+                //         template: path.join(templates, 'template.container.js'),
+                //         generated: path.join(ROOT_PATH, directory, folderName, `${name}.container.js`)
+                //       },
+                //       containerTest: {
+                //         template: path.join(templates, 'test', 'template.container.spec.js'),
+                //         generated: path.join(ROOT_PATH, directory, folderName, 'test', `${name}.container.spec.js`)
+                //       },
+                //
+                //       reducer: {
+                //         template: path.join(templates, 'template.reducer.js'),
+                //         generated: path.join(ROOT_PATH, directory, folderName, `${name}.reducer.js`)
+                //       },
+                //       reducerTest: {
+                //         template: path.join(templates, 'test', 'template.reducer.spec.js'),
+                //         generated: path.join(ROOT_PATH, directory, folderName, 'test', `${name}.reducer.spec.js`)
+                //       }
+                //     };
+                //
+                //     expect(actual).toEqual(expected);
+                //     expect(fs.existsSync(path.join(ROOT_PATH, directory, folderName))).toBeTruthy();
+                //   });
+                // });
+            });
+        });
+    });
+
+    describe('getRootPath', () => {
+        it('should return root path', () => {
+            console.log(
+                'resolvePath:',
+                resolvePath
+                    .split('/')
+                    .slice(0, 9)
+                    .join('/')
+            );
+
+            const actual = getRootPath();
+
+            const expected = `${resolvePath
+                .split('/')
+                .slice(0, 9)
+                .join('/')}/`;
 
             expect(actual).toEqual(expected);
-            expect(fs.existsSync(path.join(ROOT_PATH, directory, folderName))).toBeTruthy();
-          });
         });
-
-        describe('WITH redux', () => {
-          it('should return all templates with directories set name and create folder name, with test', () => {
-            const name = 'some_name_redux';
-            const directory = 'some_directory_redux';
-
-            const actual = getAllDirectories(name, directory, false, true);
-
-            const templates = path.join(ROOT_PATH,'templates', 'web', 'react-redux');
-            const folderName = name;
-            const expected = {
-              view: {
-                template: path.join(templates, 'template.view.js'),
-                generated: path.join(ROOT_PATH, directory, folderName, `${name}.view.js`)
-              },
-              viewTest: {
-                template: path.join(templates, 'test', 'template.view.spec.js'),
-                generated: path.join(ROOT_PATH, directory, folderName, 'test', `${name}.view.spec.js`)
-              },
-              stylesheet: {
-                template: path.join(templates, '_template.styles.scss'),
-                generated: path.join(ROOT_PATH, directory, folderName, `_${name}.styles.scss`)
-              },
-              container: {
-                template: path.join(templates, 'template.container.js'),
-                generated: path.join(ROOT_PATH, directory, folderName, `${name}.container.js`)
-              },
-              containerTest: {
-                template: path.join(templates, 'test', 'template.container.spec.js'),
-                generated: path.join(ROOT_PATH, directory, folderName, 'test', `${name}.container.spec.js`)
-              },
-
-              reducer: {
-                template: path.join(templates, 'template.reducer.js'),
-                generated: path.join(ROOT_PATH, directory, folderName, `${name}.reducer.js`)
-              },
-              reducerTest: {
-                template: path.join(templates, 'test', 'template.reducer.spec.js'),
-                generated: path.join(ROOT_PATH, directory, folderName, 'test', `${name}.reducer.spec.js`)
-              }
-            };
-
-            expect(actual).toEqual(expected);
-            expect(fs.existsSync(path.join(ROOT_PATH, directory, folderName))).toBeTruthy();
-          });
-        });
-      })
     });
-  });
-
-  describe('getRootPath', () => {
-    it('should return root path', () => {
-      console.log(
-        'resolvePath:',
-        resolvePath
-          .split('/')
-          .slice(0, 9)
-          .join('/')
-      );
-
-      const actual = getRootPath();
-
-      const expected = `${resolvePath
-        .split('/')
-        .slice(0, 9)
-        .join('/')}/`;
-
-      expect(actual).toEqual(expected);
-    });
-  });
 });
