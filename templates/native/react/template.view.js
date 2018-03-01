9355433a7b3cf84e6410e45f8eb742217c1e88ba
JP Strydom <jpstrydom8@gmail.com>
2018-03-01 16:50:01 +0200

    Update template.view.js
    Update native react template

--- templates/native/react/template.view.js
+++ templates/native/react/template.view.js
@@ -16,7 +16,7 @@ export default class TEMPLATE_UPPER_CAMEL_CASE_NAMEView extends Component {
         /*
         Add all required function binding here. If no binding is required, the constructor may be omitted.
         */
-        this.exampleHelper = this.exampleHelper.bind(this);
+        this.renderTextExample = this.renderTextExample.bind(this);
     }
 
     componentDidMount() {
@@ -25,23 +25,19 @@ export default class TEMPLATE_UPPER_CAMEL_CASE_NAMEView extends Component {
          call the load functions here in the `componentDidMount` function (See example below) -
          else, the `componentDidMount` function may be omitted.
          */
-        this.props.exampleAsyncAction();
     }
 
     render() {
-        return <View>{this.exampleHelper()}</View>;
+        return <View>{this.renderTextExample()}</View>;
     }
 
     /*
     Add all helper functions here. Remember to 'bind' functions where 'this'
     needs to be used (See example below).
     */
-    exampleHelper() {
-        this.props.exampleAction(true);
-        if (!this.props.exampleVariable) {
-            return <Text style={styles.text}>My template has been fiddled with</Text>;
-        }
-        return <Text style={styles.text}>Hello World! Welcome to my template</Text>;
+    renderTextExample() {
+        const message = `Welcome to our template view ${this.props.exampleVariable || '( ͡° ͜ʖ ͡°)'}`;
+        return <Text style={styles.text}>{message}</Text>;
     }
 }
 
@@ -51,10 +47,7 @@ This will make code completion and IDE inspection possible, and will make it eas
 in this module.
 */
 TEMPLATE_UPPER_CAMEL_CASE_NAMEView.propTypes = {
-    exampleVariable: PropTypes.bool,
-
-    exampleAction: PropTypes.func,
-    exampleAsyncAction: PropTypes.func
+    exampleVariable: PropTypes.string
 };
 
 /*
@@ -63,6 +56,6 @@ Add all the component styling here.
 const styles = StyleSheet.create({
     text: {
         fontSize: 20,
-        color: colors.nightHawk
+        textAlign: 'center'
     }
 });
